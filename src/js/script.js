832e2c6299464223de069d8cc12a3f9d69683838
Haga Junya <n0812034@yahoo.co.jp>
2024-10-12 13:08:22 +0900

    2024/1012

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
-
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    
+    
+    //ヘッダーの色の変化
+    const header = document.querySelector('.header');
+    // スクロールイベントを監視
+    $(window).on('scroll', function() {
+        if (window.scrollY > 1000) { // 1000pxを超えたらクラスを追加
+            header.classList.add('js-header');
+        } else { // 1000px未満ならクラスを削除
+            header.classList.remove('js-header');
+        }
+    });
 
 
+    jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+        // ロゴがクリックされたときにトップへスクロール
+        $('.header__logo a').on('click', function(e) {
+            e.preventDefault(); // 通常のリンク動作を無効化
+            $('html, body').animate({ scrollTop: 0 }, 'slow'); // スムーズにトップへスクロール
+        });
+    });
+    
 });
